fae223bb75488bbb9a6ade6b1f7d7ba974373834
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-13 21:47:30 +0300

    Initial commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Module defines minOperations function'''
+
+
+def minOperations(n):
+    '''calculates the fewest number of operations.
+    needed to result in exactly `n H` characters in the file
+    Args:
+        n(int): number of character `H`
+    Returns:
+        int: number of operations
+    '''
+    pass
