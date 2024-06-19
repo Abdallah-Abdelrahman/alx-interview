756a28d97cfd6a2211f8889a4efdfaa726703166
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-19 18:01:20 +0300

    Debug 0x04

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,7 +2,7 @@
 '''script that reads stdin line by line and computes metrics'''
 import signal
 from sys import stdin, exit
-from re import search, compile
+from re import compile
 
 STATUS = {
         200: 0,
@@ -31,31 +31,33 @@ def handler(signum, frame):
     exit(0)
 
 
-i = 1
-total = 0
 # signal interrupt handler
 signal.signal(signal.SIGINT, handler)
 
-for line in stdin:
-    # regex to match log line
-    ip_regex = r'^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}'
-    mid_regex = r' "GET /projects/260 HTTP/1.1" '
+if __name__ == '__main__':
+    i = 0
+    total = 0
 
-    regex = compile(ip_regex+r' - \[\S+ \S+\]'+mid_regex+r'(\d{3}) (\d+)$')
-    m = regex.match(line)
+    for line in stdin:
+        # regex to match log line
+        ip_regex = r'^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}'
+        mid_regex = r' "GET /projects/260 HTTP/1.1" '
 
-    if not m:
-        # there's no match skip line
-        continue
+        regex = compile(ip_regex+r' - \[\S+ \S+\]'+mid_regex+r'(\d{3}) (\d+)$')
+        m = regex.match(line)
 
-    _, __, ___, status_code, size = m.groups()
+        if not m:
+            # there's no match skip line
+            continue
 
-    if status_code.isnumeric and int(status_code) in STATUS:
-        STATUS[int(status_code)] += 1
-    if size.isnumeric:
-        total += int(size)
+        _, __, ___, status_code, size = m.groups()
 
-    if i % 10 == 0:
-        # print statistic
-        print_statistics(total)
-    i += 1
+        if status_code.isnumeric and int(status_code) in STATUS:
+            STATUS[int(status_code)] += 1
+        if size.isnumeric:
+            total += int(size)
+
+        if i % 10 == 0:
+            # print statistic
+            print_statistics(total)
+        i += 1
