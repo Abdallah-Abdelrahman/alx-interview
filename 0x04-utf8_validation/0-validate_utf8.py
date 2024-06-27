80443ee35ff93132b863cffeab1b558aad91f8fe
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-28 07:08:34 +0800

    Determine if data is valid 1byte `utf8`

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,5 +3,16 @@
 
 
 def validUTF8(data):
-    '''determines if a given data set represents a valid UTF-8 encoding'''
-    pass
+    '''determines if a given data set represents a valid UTF-8 encoding.
+
+    Returns:
+        bool: True if valid 1byte utf8, False otherwise
+    '''
+    for char in data:
+        # using bit-wise right shift to get MSB
+        # then mask it with one to get the value of MSB
+        # if it's 0 then it's valid 1byte utf8
+        if (char >> 8) & 1 == 1:
+            return False
+
+    return True
