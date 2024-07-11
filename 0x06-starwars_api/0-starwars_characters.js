067234adbdec73cdff199a5517f7ee4b07feecaf
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-12 07:48:16 +0800

    Debug 0x01

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,37 +1,40 @@
 #!/usr/bin/node
+
 /**
  * script that prints all characters of a Star Wars movie:
  *
- * The first positional argument passed is the Movie ID - example: 3 = “Return of the Jedi”
+ * The first positional argument passed is the Movie ID.
+ * - example: 3 = “Return of the Jedi”
  */
-const { argv: { 2: movieID } } = process;
 const request = require('request');
+const { argv: { 2: movieID } } = process;
 
 request(
   `https://swapi-api.alx-tools.com/api/films/${movieID}`,
   (err, _response, body) => {
     if (err) {
-      console.error('Error fetching film:', err);
-    } else {
-      const chars = JSON.parse(body).characters;
-
-      const promises = chars.map((url) => new Promise((resolve, reject) => {
-        request(url, (err, resp, body) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          resolve(JSON.parse(body).name);
-        })
-      }));
-
-      Promise.all(promises)
-        .then((names) => {
-          names.forEach((name) => console.log(name));
-        })
-        .catch((err) => {
-          console.error('Error fetching character:', err);
-        });
+      console.error({ err });
+      return
     }
+
+    const chars = JSON.parse(body).characters;
+
+    const promises = chars.map((url) => new Promise((resolve, reject) => {
+      request(url, (err, _resp, body) => {
+        if (err) {
+          reject(err);
+          return
+        }
+        resolve(JSON.parse(body).name);
+      })
+    }));
+
+    Promise.all(promises)
+      .then((names) => {
+        names.forEach((name) => console.log(name));
+      })
+      .catch((err) => {
+        console.error({ err });
+      });
   }
 );
