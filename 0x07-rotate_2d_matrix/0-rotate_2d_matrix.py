2623287c956b6a4fa87aca9bc55c299094028c79
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-19 10:52:59 +0800

    Fix typo

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,7 +6,7 @@ def rotate_2d_matrix(matrix):
     '''rotate it 90 degrees clockwise'''
     len_ = len(matrix)
     for i in range(len(matrix)):
-        # trasnposing the matrix
+        # transposing the matrix
         for j in range(i, len(matrix)):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
