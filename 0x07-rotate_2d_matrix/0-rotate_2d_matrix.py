59cbe7b3a5c4501cd94df86e62f81899f9fa5a2f
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-19 10:50:34 +0800

    First try to rorate

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,3 +4,14 @@
 
 def rotate_2d_matrix(matrix):
     '''rotate it 90 degrees clockwise'''
+    len_ = len(matrix)
+    for i in range(len(matrix)):
+        # trasnposing the matrix
+        for j in range(i, len(matrix)):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(len_):
+        # reverse each row
+        for j in range(len_ // 2):
+            matrix[i][j], matrix[i][len_ - j - 1] =\
+                    matrix[i][len_ - j - 1], matrix[i][j]
